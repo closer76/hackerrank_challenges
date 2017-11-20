1641614e5035205b32f5282b437ec21d41ec62cd
Kenneth Lo <closer.tw@gmail.com>
2017-11-20 18:48:05 +0800

    Add a program to test mylib.

---
+++ mylib_test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "mylib.h"
+using namespace std;
+using namespace KennethLo;
+
+const int MOD = 1000000007;
+//const int MOD = 1000000;
+
+int main(void)
+{
+    cout << power_mod(71, 927, MOD) << endl;
+
+    return 0;
+}
