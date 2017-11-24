f9c8bda581c7ccf15c3719c76abc240074fad434
Kenneth Lo <closer.tw@gmail.com>
2017-11-24 15:02:20 +0800

    [FilterElements] Use built-in functions instead of mine.

--- FilterElements.hs
+++ FilterElements.hs
@@ -6,40 +6,13 @@ formatListStr [] = "-1"
 formatListStr [x] = show x
 formatListStr (x:xs) = (show x) ++ " " ++ (formatListStr xs)
 
--- O(n^2) solution...
-{--
-filterElem :: Int -> [Int] -> ([Int], [Int]) ->([Int], [Int])
-filterElem _ [] (a, b) = (a, b)
-filterElem expect (x:xs) (a, b)
-    | expect == x = filterElem expect xs (x:a, b)
-    | otherwise   = filterElem expect xs (a, b ++ [x])
-
-solve :: Int -> [Int] -> [Int] -> [Int]
-solve _ [] result = if result == [] then [-1] else result
-solve k all@(x:xs) result = let
-    (found, remain) = filterElem x all ([], [])
-    new_result = if length found >= k
-                 then result ++ [x]
-                 else result
-    in solve k remain new_result
---}
-
-filterUnwanted :: Int -> Int -> [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
-filterUnwanted threshold count result input
-    | input == []        = if count >= threshold then result else if result == [] then [] else ys
-    | result == []       = filterUnwanted threshold 1 [x] xs
-    | fst x == fst y     = filterUnwanted threshold (count + 1) result xs
-    | count >= threshold = filterUnwanted threshold 1 (x:result) xs
-    | otherwise          = filterUnwanted threshold 1 (x:ys) xs
-    where
-        (x:xs) = input
-        (y:ys) = result
-
 solve :: Int -> [Int] -> [Int]
 solve threshold =
     map (\i -> fst i)
         . sortBy (\a b -> if snd a < snd b then LT else GT)
-        . filterUnwanted threshold 0 []
+        . map head
+        . filter (\g -> length g >= threshold)
+        . groupBy (\a b -> fst a == fst b)
         . sortBy (\a b -> if ((fst a) <= (fst b) && (snd a < snd b)) then LT else GT)
         . (flip zip) [1..]
 
